b92ec3c8ddb3542e8eedb40366aeeb4ed73eaea0
AlexMato <matontialessandro0@gmail.com>
2020-05-05 16:28:40 +0200

    exOne completato

--- exOne/index.php
+++ exOne/index.php
@@ -5,6 +5,23 @@
     <title></title>
   </head>
   <body>
-    ciao
+    <?php
+    $name = $_GET["name"];
+    $surname = $_GET["surname"];
+
+     ?>
+
+     <?php
+     echo "Ben arrivato/a " . $name . " " . $surname;
+
+      ?>
   </body>
 </html>
+
+
+
+
+
+
+<!-- - passare come argomenti in GET nome e cognome; e salutare l'interlocutore
+- passare come argomento in GET una mail e stampare un div che contenga OK se contiene un punto e una chiocciola; KO altrimenti -->
